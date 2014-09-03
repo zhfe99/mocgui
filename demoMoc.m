95b9a93f83c7fa3aa3af1049b2faee993b59d3e5
Feng Zhou <zhfe99@gmail.com>
2014-09-03 10:34:12 -0400

    store

--- demoMoc.m
+++ demoMoc.m
@@ -2,7 +2,7 @@
 %
 % History
 %   create  -  Feng Zhou (zhfe99@gmail.com), 08-30-2014
-%   modify  -  Feng Zhou (zhfe99@gmail.com), 08-30-2014
+%   modify  -  Feng Zhou (zhfe99@gmail.com), 09-03-2014
 
 clear variables;
 prSet(2); % set prompt level. a larger value (eg., 3) showing more details.
@@ -26,10 +26,14 @@ anMocap(wsMoc);
 iF = 100;
 
 % creating figure
-Ax = iniAx(1, 1, 1, [400 400]);
+fig = 2;
+Ax = iniAx(fig, 1, 1, [400 400]);
 
 % set the 3D boundary of the mocap data
 shMocG(wsMoc.QC, 'ax', Ax{1});
 
 % show the frame
 shMocF(wsMoc.QC(:, :, iF), wsMoc.Conn, wsMoc.skel);
+
+% title
+title(sprintf('Frame %d', iF));
